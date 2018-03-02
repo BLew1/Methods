52d88d3ef8c04880345617edf52663d8bf2eb92a
BLew1 <36410974+BLew1@users.noreply.github.com>
2018-03-02 08:59:59 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Get marks from user
+        int[] marks = getMarks();
+
+
+        // print marks
+        printMarks(marks);
+
+        // modify marks
+        marks = modifyMarks(marks);
+
+
+        // print marks
+        printMarks(marks);
+
+    }
+    static int[] getMarks(){
+        Scanner scan = new Scanner(System.in);
+        int size = scan.nextInt();
+        int[] marks = new int[size];
+        for (int i = 0; i < marks.length; i++) {
+            marks[i] = scan.nextInt();
+        }
+        return marks;
+
+    }
+
+    static void printMarks(int[] marks){
+        System.out.println("\nPrinting marks");
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println("marks[" + i + "] = " + marks[i]);
+        }
+    }
+
+    static int[] modifyMarks(int[] marks){
+        System.out.println("\nModifying marks");
+        for (int i = 0; i < marks.length; i++) {
+            marks[i] += 5;
+        }
+        return marks;
+    }
+
+
+}
